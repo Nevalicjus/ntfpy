03dd78bb7d52b74ba423079cc806a264b8f5396a
Nathan PERIER <periernathan2000@gmail.com>
2022-11-16 17:39:32 +0100

    Added a data structures for a message to be sent

---
+++ src/ntfpy/types/push_message.py
@@ -0,0 +1,34 @@
+from typing import Optional, MutableSequence
+
+from .actions import NTFYAction
+
+__all__ = [
+	"NTFYPushMessage"
+]
+
+class NTFYPushMessage():
+    def __init__(self, message: str, title: Optional[str] = None, priority: Optional[int] = None, 
+                       tags: Optional[MutableSequence[str]] = None, click_url: Optional[str] = None, attach: Optional[str] = None, 
+                       actions: Optional[MutableSequence[NTFYAction]] = None, email: Optional[str] = None, delay: Optional[str] = None,
+                       icon_url: Optional[str] = None):
+        self.message = message
+        self.title = title
+        self.priority = priority
+        self.tags = tags
+        self.click_url = click_url
+        self.attachement = attach
+        self.actions = actions
+        self.email = email
+        self.delay = delay
+        self.icon_url = icon_url
+    
+    def addTag(self, tag: str) :
+        if self.tags is None :
+            self.tags = []
+        self.tags.append(tag)
+    
+    def addAction(self, action: NTFYAction) :
+        if self.actions is None :
+            self.actions = []
+        self.actions.append(action)
+    
